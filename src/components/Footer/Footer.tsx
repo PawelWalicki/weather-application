ea8be1c5269465b4979bbdcc80c50f5f92296583
Pawel Walicki <pawel.walicki89@interia.pl>
2024-07-26 11:01:11 +0200

    CSS modified

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,19 @@
-export function Footer () {
+import "./Footer.css"
+export function Footer() {
 
     return (
-        <div>Project by Paweł Walicki</div>
+        <div className="footer">
+            Project by Paweł Walicki
+            <div className="icon-box">
+            <a href="https://github.com/PawelWalicki/weather-application">
+                <img className="footer-icon" src="github-icon.png"></img>
+            </a>
+            <a href="https://www.linkedin.com/in/paweł-walicki-a39b09176/">
+                <img className="footer-icon" src="linkedin-icon.png"></img>
+            </a>
+
+            </div>
+            {/* <link rel="github-icon.png" href="https://github.com/PawelWalicki/weather-application" /> */}
+        </div>
     )
 }
